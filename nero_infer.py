a713b9dc0fa51df25512945c53a095e993023b55
EX3 <100339835+EX3exp@users.noreply.github.com>
2022-12-12 20:38:48 +0900

    Update nero_infer.py

--- nero_infer.py
+++ nero_infer.py
@@ -17,7 +17,7 @@ logging.getLogger('numba').setLevel(logging.WARNING)
 project_name = "nero"
 model_path = '/content/drive/MyDrive/diffsvc/NERo_diff-svc/model_ckpt_steps_30000.ckpt'
 config_path = '/content/drive/MyDrive/diffsvc/NERo_diff-svc/config.yaml'
-set_hparams('', '', '', True, True, True, True, config_path)
+set_hparams(config_path)
 hubert_gpu = True
 svc_model = Svc(project_name,config_path,hubert_gpu, model_path)
 print('model loaded')
