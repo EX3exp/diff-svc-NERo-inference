5cf244fecae5f491d262e69db0d2f89648d71cf6
EX3 <100339835+EX3exp@users.noreply.github.com>
2022-12-12 20:34:19 +0900

    Update nero_infer.py

--- nero_infer.py
+++ nero_infer.py
@@ -1,4 +1,4 @@
-from utils.hparams import hparams
+from utils.hparams import hparams, load_config
 from preprocessing.data_gen_utils import get_pitch_parselmouth,get_pitch_crepe
 import numpy as np
 import matplotlib.pyplot as plt
@@ -10,12 +10,14 @@ from infer import *
 import logging
 from infer_tools.infer_tool import *
 
+
 logging.getLogger('numba').setLevel(logging.WARNING)
 
 # 工程文件夹名，训练时用的那个
 project_name = "nero"
 model_path = '/content/drive/MyDrive/diffsvc/NERo_diff-svc/model_ckpt_steps_30000.ckpt'
 config_path = '/content/drive/MyDrive/diffsvc/NERo_diff-svc/config.yaml'
+load_config(config_path)
 hubert_gpu = True
 svc_model = Svc(project_name,config_path,hubert_gpu, model_path)
 print('model loaded')
