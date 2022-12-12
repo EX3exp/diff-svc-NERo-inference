0fcd45de3cf1d02be9cf41baf2b0bcb29edb356d
EX3 <100339835+EX3exp@users.noreply.github.com>
2022-12-12 20:54:17 +0900

    Update hparams.py

--- utils/hparams.py
+++ utils/hparams.py
@@ -1,8 +1,8 @@
 import yaml
 
-global hparams
 
 def set_hparams(config_yaml_path: str) -> dict:
     with open(config_yaml_path, encoding='UTF-8') as f:
+        global hparams
         hparams = yaml.load(f, Loader=yaml.FullLoader)
     print('loaded config.yaml...')
